a22089cc8e8003b1e99759676f0d9f19ab5c8d5c
Aleyna Cankat <aleynacankat0@gmail.com>
2023-04-17 22:07:18 +0300

    day1day2update

---
+++ day1.py
@@ -0,0 +1,6 @@
+#variables
+
+age = 50
+print(age*2)
+
+# data type / yorum satiri
---
+++ day2.py
@@ -0,0 +1,39 @@
+
+print(type(10))
+
+# int ile tamsayilar integer ile calismaktadir.
+
+print(type(-100))
+
+print(type(3.14))
+
+#float = floating point
+
+pi = 3.14
+
+print(2*pi)
+
+x = 10
+#sayi tiplerinin type'ları vardir.
+myFloat = 1.55785757 #myFloat camelCase snake_case
+
+print(type(myFloat + 5))
+
+type(print(int(40/4)))
+type(print((4.0/4.0)))
+
+
+type(print(2**2)) #üssünü al
+type(print(2%2)) #kalanı bul (remainder)
+
+name = "aleyna"
+
+print(type(name))
+#name.capitalize() #ilk harfini büyük yazdırır sondaki parantez capitalize() metodunuz calistir demek.
+print(name.capitalize())
+nameUpper = name.upper() #yeni degiskene esitlemek onemli
+print(nameUpper)
+
+print("aleyna \ncankat") #new line (escape characters)
+print("aleyna \ncankat") #\t -> tab
+print("aleyna\tcankat")
