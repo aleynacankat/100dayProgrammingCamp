f61ca06b430703fd609113f3bb4937fd2022be05
Aleyna Cankat <aleynacankat0@gmail.com>
2023-05-11 22:30:37 +0300

    updateOOP

--- tekrarMac.py
+++ tekrarMac.py
@@ -35,3 +35,37 @@ print(type(aleyna))
 print(aleyna.age)
 
 type(aleyna)
+
+
+class Dog():
+
+    year = 7
+
+    def __init__(self, age = 5):
+        self.age = age
+        self.dogHumanAge = age * self.year
+        print("dog instance")
+
+    def humanAge(self):
+        return self.age * Dog.year #Dog.year = self.year
+
+myDog = Dog(3)
+
+print(myDog.humanAge())
+
+#Inheritance, Encapsulation, Abstraction, Polymorphism
+#oop fundamentals
+
+class Musician():
+
+    def __init__(self):
+        print("musician class")
+
+    def test1(self):
+        print("test1")
+
+    def test2(self):
+        print("test2")
+
+aleyna = Musician("Cankat")
+
