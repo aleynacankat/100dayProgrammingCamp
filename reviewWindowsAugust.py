6ef45c040af0de4756debc9d873fda3ca619c5ca
Aleyna Cankat <aleynacankat0@gmail.com>
2023-08-05 23:10:01 +0300

    review

---
+++ reviewWindowsAugust.py
@@ -0,0 +1,39 @@
+#name = input("What is your name: ")
+#print("Welcome again to the analysis" + " " + name)
+
+#Numbers: int / float / complex
+num = 100
+print(type(num))
+
+num2 = 99.99
+print(type(num2))
+
+expression1 = num * 10
+print(type(expression1))
+
+expression2 = num + num2
+print(type(expression2))
+
+#Math Functions
+print(round(2.1))
+
+print(round(5.9))
+
+print(abs(-34))
+
+name = "Python"
+name2 = "Pyhton"
+name3 = """This is a very long string and supports"""
+name4 = 'Hello \"Rockstar Programmer\"'
+
+print(type(name))
+print(type(name2))
+print(type(name3))
+print((name4))
+
+userName = "Aleyna"
+age= 23
+print(userName + " " +str(age))
+
+
+
